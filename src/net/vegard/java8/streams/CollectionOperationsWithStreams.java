21d818e4e7bae528f04ed832657e088021525922
Vegard Skjefstad <vs@forskningsradet.no>
2015-06-04 15:04:36 +0200

    java.util.stream example code.

---
+++ src/net/vegard/java8/streams/CollectionOperationsWithStreams.java
@@ -0,0 +1,16 @@
+package net.vegard.java8.streams;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CollectionOperationsWithStreams {
+
+public static void getMeanOfAllDoublesGreaterThanZero(final List<Double> doubles) {
+    doubles.stream().filter(d -> d > 0).mapToDouble(Double::new).average().ifPresent(System.out::println);
+}
+
+    public static void main(final String [] args) {
+        getMeanOfAllDoublesGreaterThanZero(Arrays.asList(1.2d, 6.22d, 4.2d, -8.4d, 13.37d));
+    }
+
+}
---
+++ src/net/vegard/java8/streams/CollectionOperationsWithoutStreams.java
@@ -0,0 +1,26 @@
+package net.vegard.java8.streams;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CollectionOperationsWithoutStreams {
+
+    public static void getMeanOfAllDoublesGreaterThanZero(final List<Double> doubles) {
+
+        int numberOfDoubles = 0;
+        double doublesSum = 0;
+        for (final Double aDouble : doubles) {
+            if (aDouble > 0) {
+                numberOfDoubles++;
+                doublesSum += aDouble;
+            }
+        }
+
+        System.out.println(new Double(doublesSum / numberOfDoubles));
+    }
+
+    public static void main(final String[] args) {
+        getMeanOfAllDoublesGreaterThanZero(Arrays.asList(1.2d, 6.22d, 4.2d, -8.4d, 13.37d));
+    }
+
+}
