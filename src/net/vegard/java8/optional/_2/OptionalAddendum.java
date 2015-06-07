0e3a675e24a292a12480a7c6bffd2779dde586be
Vegard Skjefstad <vegard@vegard.net>
2015-06-07 11:56:45 +0200

    Added a better Optional example, but will keep the old one for reference.

---
+++ src/net/vegard/java8/optional/_2/OptionalAddendum.java
@@ -0,0 +1,28 @@
+package net.vegard.java8.optional._2;
+
+import java.util.Optional;
+
+public class OptionalAddendum {
+
+    public static Optional<String> getFullName(final String username) {
+        if ("th".equals(username)) {
+            return Optional.of("Tony Hawk");
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public static void main(final String[] args) {
+        Optional<String> fullName = getFullName("th");
+
+        if (fullName.isPresent()) {
+            System.out.println(fullName.get());
+        } else {
+            System.out.println("No user found.");
+        }
+
+        System.out.println(getFullName(null).orElse("No user found"));
+        System.out.println(getFullName("ht").orElse("No user found"));
+    }
+
+}
